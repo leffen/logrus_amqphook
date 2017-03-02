44cfcffd8058a091bc78f59529b68a1558ff57f2
leffen <leffen@gmail.com>
2017-03-02 14:03:13 +0100

    Updated test

--- amqphook_test.go
+++ amqphook_test.go
@@ -2,14 +2,13 @@ package logrus_amqphook
 
 import (
 	"github.com/Sirupsen/logrus"
-	"github.com/Sirupsen/logrus/formatters/logstash"
 	"testing"
 	"time"
 )
 
 func TestHook(t *testing.T) {
-	hook := NewAmqpHook("TestApp", "amqps://ca.paxtonagent.logger:k7qRMWe5wa@mq1.cloudaccess.io", "ca.logging.e.prod", "logrus.")
-	logrus.SetFormatter(new(logstash.LogstashFormatter))
+	hook := NewAmqpHook( "amqps://ca.paxtonagent.logger:k7qRMWe5wa@mq1.cloudaccess.io", "ca.logging.e.prod","")
+	logrus.SetFormatter(new(LogstashFormatter))
 	logrus.AddHook(hook)
 	logrus.Errorf("Something broke...")
 	time.Sleep(1 * time.Second)
